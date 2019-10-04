8ec6a36ea1f43a26e63bda7784c93dbe623e4564
ISSuh <yoyus5@naver.com>
2019-10-04 16:15:28 +0000

    add logger

--- include/mrm/server.hpp
+++ include/mrm/server.hpp
@@ -3,9 +3,16 @@
 
 // std
 #include <iostream>
+#include <memory>
+#include <thread>
+#include <csignal>
 
 // casablanca
 #include <cpprest/http_listener.h>
+
+// log
+#include <spdlog/spdlog.h>
+
 
 using namespace web::http::experimental::listener;
 using namespace web::http;
@@ -14,32 +21,29 @@ using namespace web;
 namespace mrm
 {
 
-class MrmServer
+class MrmApiServer
 {
 public:
-	MrmServer(const utility::string_t &address) : m_address(address) {}
-
-	~MrmServer()
-	{
-		std::cout << "Terminating." << std::endl;
+	MrmApiServer(const utility::string_t &address) : m_address(address){
+		m_consoleLogger = spdlog::get("MRM_APIServer");
 	}
 
 	bool Init()
-	{
-		std::cout << "Initialize listener." << std::endl;
+	{	
+		m_consoleLogger->info("Initialize listener");
 		m_listener = std::unique_ptr<http_listener>(new http_listener(m_address));
 
-		std::cout << "Regist handle." << std::endl;
-		m_listener->support(methods::GET, std::bind(&MrmServer::Get,
+		m_consoleLogger->info("Regist handle");
+		m_listener->support(methods::GET, std::bind(&MrmApiServer::Get,
 													this,
 													std::placeholders::_1));
-		m_listener->support(methods::POST, std::bind(&MrmServer::Post,
+		m_listener->support(methods::POST, std::bind(&MrmApiServer::Post,
 													 this,
 													 std::placeholders::_1));
-		m_listener->support(methods::PUT, std::bind(&MrmServer::Put,
+		m_listener->support(methods::PUT, std::bind(&MrmApiServer::Put,
 													this,
 													std::placeholders::_1));
-		m_listener->support(methods::DEL, std::bind(&MrmServer::Delete,
+		m_listener->support(methods::DEL, std::bind(&MrmApiServer::Delete,
 													this,
 													std::placeholders::_1));
 
@@ -48,13 +52,13 @@ public:
 
 	void OpenWait()
 	{
-		std::cout << "Listener Wait." << std::endl;
+		m_consoleLogger->info("Listener Wait");
 		m_listener->open().wait();
 	}
 
 	void Run()
 	{
-		std::cout << "Running MRM Server" << std::endl;
+		m_consoleLogger->info("Running MRM API Server");
 
 		while (true)
 		{
@@ -62,8 +66,15 @@ public:
 		}
 	}
 
+	void Clear()
+	{
+		m_consoleLogger->info("Terminating");
+	}
+
 private:
-	// Definde Get method 
+	// Define GET, POST, PUT, DELETE Handler
+
+	// Define Get method 
 	void Get(http_request req)
 	{
 		auto http_get_vars = uri::split_query(req.request_uri().query());
@@ -72,22 +83,24 @@ private:
 		test(req, status_codes::OK, json::value::string(U("TEST")));
 	}
 
-	// Definde Post method 
+	// Define Post method 
 	void Post(http_request req)
 	{
 	}
 
-	// Definde Put method 
+	// Define Put method 
 	void Put(http_request req)
 	{
 	}
 
-	// Definde Delete method 
+	// Define Delete method 
 	void Delete(http_request req)
 	{
 	}
 
 private:
+	// Define Main Logic method
+
 	void test(const http_request &request, const status_code &status, const json::value &response)
 	{
 		json::value resp;
@@ -103,13 +116,17 @@ private:
 		request.reply(status, resp);
 	}
 
-public:
 private:
 	// Casablanca
 	std::unique_ptr<http_listener> m_listener;
 
 	// std
 	utility::string_t m_address;
+
+	//log
+	std::shared_ptr<spdlog::logger> m_consoleLogger;
+	std::shared_ptr<spdlog::logger> m_fileLogger;
+
 };
 
 } // namespace mrm
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,52 @@
-#define _CRT_SECURE_NO_DEPRECATE
-
-#include <cpprest/http_listener.h>
+// std
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <thread>
-#include <chrono>
-#include <ctime>
 
+// casablanca
+#include <cpprest/http_listener.h>
+
+// mrm
 #include <mrm/server.hpp>
 
-using namespace web::http::experimental::listener;
-using namespace web::http;
-using namespace web;
+// log
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/daily_file_sink.h>
 
 int main()
 {
-	mrm::MrmServer server(U("http://0.0.0.0:9090/api"));
+
+	// Create Logging
+	auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	auto apiServer_consoleLogger = std::make_shared<spdlog::logger>("MRM_APIServer", consoleSink);
+	auto messageBroker_consoleLogger  = std::make_shared<spdlog::logger>("MRM_MessageBroker",  consoleSink);
+	consoleSink->set_level(spdlog::level::debug);
+
+    // auto fileSink = std::make_shared<spdlog::sinks::daily_file_sink_mt>("logfile", 23, 59);		
+	// auto apiServer_fileLogger = std::make_shared<spdlog::logger>("apiServer_fileLogger", fileSink);
+	// auto messageBroker_fileLogger  = std::make_shared<spdlog::logger>("messageBroker_fileLogger",  fileSink);
+	// fileSink->set_level(spdlog::level::debug);
+	
+	spdlog::logger mainLogger("MRM_MainLogger", {consoleSink/*, fileSink*/});
+	mainLogger.set_level(spdlog::level::debug);
+
+	spdlog::register_logger(apiServer_consoleLogger);
+	spdlog::register_logger(messageBroker_consoleLogger);
+	// spdlog::register_logger(apiServer_fileLogger);
+	// spdlog::register_logger(messageBroker_fileLogger);
+
+	spdlog::set_default_logger(std::make_shared<spdlog::logger>("MRM_MainLogger", spdlog::sinks_init_list({consoleSink/*, fileSink*/})));
+
+
+	// MRM API Server 
+	mainLogger.info("RUNNING PROCESS");
+	mrm::MrmApiServer server(U("http://0.0.0.0:9090/api"));
 	server.Init();
 	server.OpenWait();
 
 	server.Run();
 
-	return 0;
+	server.Clear();
+	
+	mainLogger.info("STOP PROCESS");
+	spdlog::shutdown();
 }
